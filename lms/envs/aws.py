5a0f314aa4848d456621e57c27fca881ec4181eb
Cory Lee <cory@edx.org>
2018-09-20 14:57:46 -0400

    Add logging

--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -26,6 +26,7 @@ from .common import *
 from openedx.core.lib.derived import derive_settings
 from openedx.core.lib.logsettings import get_logger_config
 import os
+import logging
 
 from path import Path as path
 from xmodule.modulestore.modulestore_settings import convert_module_store_setting_if_needed
@@ -1104,3 +1105,6 @@ plugin_settings.add_plugins(__name__, plugin_constants.ProjectType.LMS, plugin_c
 ########################## Derive Any Derived Settings  #######################
 
 derive_settings(__name__)
+
+logging.config.dictConfig(LOGGING)
+logging.warn('DEPRECATION WARNING: aws.py has been deprecated, you should use production.py instead.')
