d36638713e247945af9e2e2d422ccaf24b41e32f
John Eskew <jeskew@edx.org>
2018-09-24 19:44:59 -0400

    Check for retired usernames as well as existing ones in validation.

--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -233,6 +233,13 @@ def is_username_retired(username):
         raise
 
 
+def username_exists_or_retired(username):
+    """
+    Check a username against the User model for existence -or- retirement.
+    """
+    return User.objects.filter(username=username).exists() or is_username_retired(username)
+
+
 def is_email_retired(email):
     """
     Checks to see if the given email has been previously retired
